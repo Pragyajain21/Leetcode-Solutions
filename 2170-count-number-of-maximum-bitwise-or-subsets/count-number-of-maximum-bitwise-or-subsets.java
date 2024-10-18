caba8959eedc3483dd8deadfd5e3756f3efb7b52
Pragya Jain <118589578+Pragyajain21@users.noreply.github.com>
2024-10-18 11:20:20 +0530

    Time: 41 ms (41.71%) | Memory: 62.3 MB (5.26%) - LeetSync

--- 2170-count-number-of-maximum-bitwise-or-subsets/count-number-of-maximum-bitwise-or-subsets.java
+++ 2170-count-number-of-maximum-bitwise-or-subsets/count-number-of-maximum-bitwise-or-subsets.java
@@ -1,5 +1,5 @@
 class Solution {
-    public int solve(int i,int nums[],int currOR,int maxOR)
+    public int solve(int i,int nums[],int currOR,int maxOR,int dp[][])
     {
         if(i==nums.length)
         {
@@ -8,17 +8,26 @@ class Solution {
             else
             return 0;
         }
-        int take = solve(i+1,nums,(currOR | nums[i]), maxOR);
-        int nottake = solve(i+1,nums,currOR,maxOR);
-        return take + nottake;
+        if(dp[i][currOR]!=-1)
+        return dp[i][currOR];
+
+        int take = solve(i+1,nums,(currOR | nums[i]), maxOR,dp);
+        int nottake = solve(i+1,nums,currOR,maxOR,dp);
+        return dp[i][currOR]= take + nottake;
 
     }
     public int countMaxOrSubsets(int[] nums) {
+        int n = nums.length;
         int maxOR = 0;
         for(int i=0;i<nums.length;i++){
              maxOR = (maxOR | nums[i]);
         }
-        int ans = solve(0,nums,0,maxOR);
+        int dp[][]=new int[n][maxOR+1];
+
+        for(int i=0;i<n;i++){
+            Arrays.fill(dp[i],-1);
+        }
+        int ans = solve(0,nums,0,maxOR,dp);
         return ans;
     }
 }
