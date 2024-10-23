fac6d1173ffac270287b5b8110fbf94c9076a42c
Pragya Jain <118589578+Pragyajain21@users.noreply.github.com>
2024-10-23 20:09:02 +0530

    Time: 0 ms (100.00%) | Memory: 43.3 MB (48.34%) - LeetSync

--- 2341-count-prefixes-of-a-given-string/count-prefixes-of-a-given-string.java
+++ 2341-count-prefixes-of-a-given-string/count-prefixes-of-a-given-string.java
@@ -1,19 +1,25 @@
 class Solution {
     public int countPrefixes(String[] words, String s) {
      int n = words.length;
-     HashMap<String,Integer> map = new HashMap<>();
+    //  HashMap<String,Integer> map = new HashMap<>();
 
-     for(int i=0;i<n;i++){
-       map.put(words[i],map.getOrDefault(words[i],0)+1);
-     }
-     int cnt =0;
-     for(int i= 1;i<=s.length();i++){
-        String str = s.substring(0,i);
-        if(map.containsKey(str))
-        cnt+=map.get(str);
+    //  for(int i=0;i<n;i++){
+    //    map.put(words[i],map.getOrDefault(words[i],0)+1);
+    //  }
+    //  int cnt =0;
+    //  for(int i= 1;i<=s.length();i++){
+    //     String str = s.substring(0,i);
+    //     if(map.containsKey(str))
+    //     cnt+=map.get(str);
 
-     }
-     return cnt;
+    //  }
+    //  return cnt;
+    int cnt = 0;
+    for(String word : words){
+        if(s.startsWith(word))
+        cnt++;
+    } 
+    return cnt;
 
 
         
